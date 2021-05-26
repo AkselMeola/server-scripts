cba53f6c6b6f60406aeb17f76170c722837678db
Aksel Meola <aksel@meola.eu>
2021-05-27 00:05:40 +0300

    Mysql script

---
+++ mysql/mysql_backup.sh
@@ -0,0 +1,98 @@
+#! /bin/bash
+
+########################################################################### 
+# Mysql Backup script v1.0
+# https://github.com/AkselMeola/server-scripts
+#
+# Dumps all databases to a dated folder.
+# Dumps are gzipped.
+# Backups are kept for certain amount of days and removed when expired.
+#
+# Configuration
+# ------------
+#
+# Mysql client looks for host and credentials in ~/.my.cnf
+# For more see MySQL documentation about "option files"
+# (Example: https://dev.mysql.com/doc/refman/5.7/en/option-files.html)
+#
+# NB! Do not pass user credentials on command line! 
+# (the command string may appear visible by ps or htop commands)
+#
+#
+# Installation
+# ------------
+#
+# 1. Download script and place it into your executables directory. 
+#
+#	Example: TODO: 
+#
+# 2. Setup a cron job to run daily
+#
+#	Example: TODO:
+#
+#
+#
+########################################################################### 
+#
+# Configuration variables
+#
+# How many days to keep backups 
+KEEP_BACKUPS_DAYS=30
+# Directory where backups are stored (without trailing slash)
+BACKUP_DIR="~/backups/database"
+# Skip schemas from backup
+SKIP_SCHEMAS=("mysql" "information_schema" "performance_schema" "sys")
+
+#
+# !!! Do not edit below unless you know what you are doing. !!!
+#
+# Variables
+DEL_TIMESTAMP=$(date -d "-$KEEP_BACKUPS_DAYS days" +"%Y-%m-%d")
+CUR_TIMESTAMP=$(date +"%Y-%m-%d")
+MYSQL_OPTS=""
+
+# Required executables
+EXEC_MYSQL=$(which mysql)
+EXEC_MYSQLDUMP=$(which mysqldump)
+
+# Create backups dated paths
+BACKUP_DIR="$BACKUP_DIR/$CUR_TIMESTAMP"
+EXPIRED_BACKUP_DIR="$BACKUP_DIR/$DEL_TIMESTAMP"
+
+# Create dated backups directory
+echo "[$(date '+%Y-%m-%d %H:%M:%S')] creating directory $BACKUP_DIR"
+mkdir -p $BACKUP_DIR
+if [[ $? != 0 ]]; then
+  echo "Unable to create backups directory at $BACKUP_DIR" 
+  exit 1
+fi
+
+# Remove expired backup directory
+echo "[$(date '+%Y-%m-%d %H:%M:%S')] removing expired backups directory $EXPIRED_BACKUP_DIR"
+rm -rf $EXPIRED_BACKUP_DIR
+
+
+echo "[$(date '+%Y-%m-%d %H:%M:%S')] querying for schemas to back up ..."
+databases=$($EXEC_MYSQL $MYSQL_OPTS -e "SELECT schema_name FROM information_schema.schemata ORDER BY schema_name ASC;" | grep -Ev "(schema_name)")
+
+#Loop schemas and dump to file
+echo "[$(date '+%Y-%m-%d %H:%M:%S')] starting backup process:"
+for db in $databases; do
+   if [[ " ${SKIP_SCHEMAS[*]} " == *" $db "* ]]; then
+     echo "[$(date '+%Y-%m-%d %H:%M:%S')] Skipping db: $db"
+     continue
+   fi
+
+   echo "[$(date '+%Y-%m-%d %H:%M:%S')] Backing up: $db"
+
+   $EXEC_MYSQLDUMP $MYSQL_OPTS --force --no-create-db --opt $db | gzip > "$BACKUP_DIR/$db.gz"
+done
+
+# Report on backups directory size
+BACKUP_SIZE=`du -sh $BACKUP_DIR | cut -d'/' -f1`
+echo "[$(date '+%Y-%m-%d %H:%M:%S')] backup size for $BACKUP_DIR: $BACKUP_SIZE"
+
+# We are done
+echo "[$(date '+%Y-%m-%d %H:%M:%S')] backup process finished. All done."
+
+exit 0
